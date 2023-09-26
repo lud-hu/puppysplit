1820d9fcca338322642927dd1ec7039a3b28bd85
Ludwig Hubert <ludwig.hubert@gmail.com>
2023-09-26 20:17:00 +0200

    fix persistence bug

--- src/debtorSelectionPersistor.js
+++ src/debtorSelectionPersistor.js
@@ -2,17 +2,16 @@
 htmx.defineExtension("debtorSelectionPersistor", {
   onEvent: function (name, evt) {
     if (name === "htmx:afterProcessNode") {
+      const puppyId = window.location.href.split("/puppies/")[1].split("/")[0];
+
       // Restore user if present in local storage
-      const userId = localStorage.getItem("puppy1");
+      const userId = localStorage.getItem("puppy" + puppyId);
       if (userId) {
         evt.target.value = userId;
       }
 
       // Save user on selection to local storage
       evt.target.addEventListener("change", (event) => {
-        const puppyId = window.location.href
-          .split("/puppies/")[1]
-          .split("/")[0];
         localStorage.setItem("puppy" + puppyId, event.target.value);
       });
     }
